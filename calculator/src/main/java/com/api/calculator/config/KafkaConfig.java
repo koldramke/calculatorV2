1866833a8c193c09f7f3d2cbca0de945e6915ff2
koldramke <jose_egrl@hotmail.com>
2024-12-18 18:41:02 +0000

    all kafka files created, added, contructed and all working

---
+++ calculator/src/main/java/com/api/calculator/config/KafkaConfig.java
@@ -0,0 +1,29 @@
+package com.api.calculator.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Map;
+import org.springframework.kafka.core.ProducerFactory;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(Map.of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class));
+    }
+
+}
---
+++ calculator/src/main/java/com/api/calculator/controller/KafkaController.java
@@ -0,0 +1,19 @@
+package com.api.calculator.controller;
+
+import com.api.calculator.service.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+    @Autowired
+    private KafkaProducerService kafkaProducerService;
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        kafkaProducerService.sendMessage("example-topic", message);
+        return "MESSAGE SENT: " + message;
+    }
+}
